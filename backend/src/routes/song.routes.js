19c8209d5c9c7046f99c9fb5f00a7954895c097f
rudra-netizen <rudrakshtiwari972@gmail.com>
2026-04-15 18:21:20 +0530

    Imagekit integrated Successfully

--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const songModel = require("../models/song.model");
 const multer = require("multer");
+const { uploadFile } = require("../service/storage.service");
 
 const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() }); // storage in temporary memory
 
-router.post("/songs", upload.single("file"), (req, res) => {
+router.post("/songs", upload.single("file"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
+
+  const fileData = await uploadFile(req.file);
+  console.log(fileData);
   res.status(201).json({
     message: "Song created successfully",
+    fileData,
   });
 });
 
---
+++ backend/src/service/storage.service.js
@@ -0,0 +1,28 @@
+const ImageKit = require("imagekit");
+const mongoose = require("mongoose");
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
+
+function uploadFile(file) {
+  return new Promise((resolve, reject) => {
+    imagekit.upload(
+      {
+        file: file.buffer,
+        fileName: mongoose.Types.ObjectId().toString(),
+        folder: "apne-gaane",
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      },
+    );
+  });
+}
+
+module.exports = { uploadFile };
